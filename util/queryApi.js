367f7325f6b744bc41fb78b51dde4a2a0a1edda3
Ahmad Al Hariri <100470190+ahmadmhd357@users.noreply.github.com>
2023-02-15 01:58:11 +0300

    decrease the max token

--- util/queryApi.js
+++ util/queryApi.js
@@ -6,7 +6,7 @@ const query = async (message) => {
       model: "text-davinci-003",
       prompt: message,
       temperature: 1,
-      max_tokens: 1000,
+      max_tokens: 200,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
