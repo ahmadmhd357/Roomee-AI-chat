52737f4a089cb6c4215a5bb4ef41112e0a89e8c1
AHMAD ALHARIRI <100470190+ahmadmhd357@users.noreply.github.com>
2023-02-15 12:26:04 +0300

    Update queryApi.js

--- util/queryApi.js
+++ util/queryApi.js
@@ -5,7 +5,7 @@ const query = async (message) => {
     .createCompletion({
       model: "text-curie-001",
       prompt: message,
-      temperature: 0.7,
+      temperature: 1,
       max_tokens: 1000,
       top_p: 1,
       frequency_penalty: 0,
